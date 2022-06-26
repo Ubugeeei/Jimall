b40a9577cb1c8edcb1d644c14157b8640bd40291
Ubugeeei <ubuge1122@gamil.com>
2022-06-26 22:16:54 +0900

    tweak examples

--- src/main.rs
+++ src/main.rs
@@ -6,36 +6,36 @@ fn main() {
      * four arithmetic operations
      */
     // add
-    println!("expr=\"(+ 10 20)\"\n result=\"{:?}\"\n", eval("(+ 10 20)"));
+    println!("expr = (+ 10 20)\nresult = {:?}\n", eval("(+ 10 20)"));
 
     // sub
-    println!("expr=\"(- 10 20)\"\n result=\"{:?}\"\n", eval("(- 10 20)"));
+    println!("expr = (- 10 20)\nresult = {:?}\n", eval("(- 10 20)"));
 
     // sub (signed)
-    println!("expr=\"(- 20 10)\"\n result=\"{:?}\"\n", eval("(- 20 10)"));
+    println!("expr = (- 20 10)\nresult = {:?}\n", eval("(- 20 10)"));
 
     // mul
-    println!("expr=\"(* 20 10)\"\n result=\"{:?}\"\n", eval("(* 20 10)"));
+    println!("expr = (* 20 10)\nresult = {:?}\n", eval("(* 20 10)"));
 
     // div int
-    println!("expr=\"(/ 20 10)\"\n result=\"{:?}\"\n", eval("(/ 20 10)"));
+    println!("expr = (/ 20 10)\nresult = {:?}\n", eval("(/ 20 10)"));
 
     // div (float)
-    println!("expr=\"(/ 10 20)\"\n result=\"{:?}\"\n", eval("(/ 10 20)"));
+    println!("expr = (/ 10 20)\nresult = {:?}\n", eval("(/ 10 20)"));
 
     /*
      * list operations
      */
     println!(
-        "expr=\"(cdr '(10 20 30))\" \nresult=\"{:?}\"\n",
+        "expr = (cdr '(10 20 30)) \nresult = {:?}\n",
         eval("(cdr '(10 20 30))")
     );
     println!(
-        "expr=\"(car '(10 20 30))\" \nresult=\"{:?}\"\n",
+        "expr = (car '(10 20 30)) \nresult = {:?}\n",
         eval("(car '(10 20 30))")
     );
     println!(
-        "expr=\"(car (cdr '(10 20 30)))\" \nresult=\"{:?}\"\n",
+        "expr = (car (cdr '(10 20 30))) \nresult = {:?}\n",
         eval("(car (cdr '(10 20 30)))")
     );
 }
